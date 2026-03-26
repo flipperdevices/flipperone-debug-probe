c24162912207d41017199f0873b563ccb20c7075
SkorP <skorpionm@yandex.ru>
2026-03-26 21:48:55 +0400

    init gpio input

--- targets/f150/furi_hal/furi_hal_resources.c
+++ targets/f150/furi_hal/furi_hal_resources.c
@@ -1,17 +1,17 @@
 #include <furi_hal_resources.h>
 
 void furi_hal_resources_init(void) {
-    furi_hal_gpio_init_simple(&gpio_mcu_m40, GpioModeOutputPushPull);
-    furi_hal_gpio_init_simple(&gpio_mcu_m41, GpioModeOutputPushPull);
-    furi_hal_gpio_init_simple(&gpio_cpu_d3, GpioModeOutputPushPull);
-    furi_hal_gpio_init_simple(&gpio_cpu_d2, GpioModeOutputPushPull);
+    furi_hal_gpio_init_simple(&gpio_mcu_m40, GpioModeInput);
+    furi_hal_gpio_init_simple(&gpio_mcu_m41, GpioModeInput);
+    furi_hal_gpio_init_simple(&gpio_cpu_d3, GpioModeInput);
+    furi_hal_gpio_init_simple(&gpio_cpu_d2, GpioModeInput);
     furi_hal_gpio_init_simple(&gpio_cpu_reset, GpioModeOutputOpenDrain);
     furi_hal_gpio_init_simple(&gpio_mcu_reset, GpioModeOutputOpenDrain);
 
-    furi_hal_gpio_write(&gpio_mcu_m40, false);
-    furi_hal_gpio_write(&gpio_mcu_m41, false);
-    furi_hal_gpio_write(&gpio_cpu_d3, false);
-    furi_hal_gpio_write(&gpio_cpu_d2, false);
+    // furi_hal_gpio_write(&gpio_mcu_m40, false);
+    // furi_hal_gpio_write(&gpio_mcu_m41, false);
+    // furi_hal_gpio_write(&gpio_cpu_d3, false);
+    // furi_hal_gpio_write(&gpio_cpu_d2, false);
     furi_hal_gpio_write_open_drain(&gpio_cpu_reset, true);
     furi_hal_gpio_write_open_drain(&gpio_mcu_reset, true);
 }
