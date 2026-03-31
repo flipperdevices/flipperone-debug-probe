a9d6506548000e41016bb4844f77f7e3e5b49adc
SG <who.just.the.doctor@gmail.com>
2026-03-31 15:33:01 +0100

    Version: generate defines on build

--- lib/furi/version/version.inc.h
+++
@@ -1,8 +0,0 @@
-#define GIT_COMMIT "0daa7237"
-#define GIT_BRANCH "XXXX"
-#define VERSION "unknown"
-#define BUILD_DIRTY 1
-#define GIT_ORIGIN "https://github.com/XXXXX.git"
-#define BUILD_DATE "2026-03-25"
-#define TARGET 150
-#define FIRMWARE_ORIGIN "Official"
